2a3fa995fbcfdb141cff1f000a01631de8335b16
paulinnha <paulafaverofarias17@hotmail.com>
2022-04-01 09:09:30 -0300

    pinguim

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,13 @@
 var somar = require("./módulos/somar");
-
 console.log(somar(20,90));
+
+
+var subtrair = require("./módulos/subtrair");
+console.log(subtrair(90,20));
+
+
+var dividir = require("./módulos/dividir");
+console.log(dividir(20,10));
+
+var multiplicar = require("./módulos/multiplicar");
+console.log(multiplicar(20,10));
--- módulos/modulo.js
+++
@@ -1,3 +0,0 @@
-exports.dataehora = function () {
-  return Date();
-};
